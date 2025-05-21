1162618b615b68005ca5cc862019e4644784e7aa
Divya Roshinkumar <68938920+divz-tech@users.noreply.github.com>
2025-05-21 21:15:42 +1200

    feat: regenerate open api (#5)
    Co-authored-by: Divya Roshinkumar <divya.roshinkumar@halter.co.nz>

---
+++ src/shared/api/openapi-client/models/index.ts
@@ -0,0 +1,19 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './Battery';
+export * from './CooktopEnum';
+export * from './Emissions';
+export * from './EmissionsValues';
+export * from './Household';
+export * from './LocationEnum';
+export * from './Opex';
+export * from './OpexValues';
+export * from './Recommendation';
+export * from './RecommendationActionEnum';
+export * from './Savings';
+export * from './Solar';
+export * from './SpaceHeatingEnum';
+export * from './UpfrontCost';
+export * from './Vehicle';
+export * from './VehicleFuelTypeEnum';
+export * from './WaterHeatingEnum';
